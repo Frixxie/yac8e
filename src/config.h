7e752540b9e80964997b9e77249809ba112a856a
Frixxie <freddahf@outlook.com>
2020-06-28 22:35:30 +0000

    Added config and global header

---
+++ src/config.h
@@ -0,0 +1,17 @@
+#ifndef CONFIG_H_
+#define CONFIG_H_
+
+#include <stdlib.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <assert.h>
+#include <getopt.h>
+#include <string.h>
+#include <SDL2/SDL.h>
+
+enum
+{
+   INPUT_FILELEN = 256,
+};
+
+#endif
