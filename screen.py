4c5a9c198f3a7d89c8f53394901b14aaca30fb95
Fredrik <freddahf@outlook.com>
2021-02-27 22:54:35 +0100

    minor mods

--- screen.py
+++ screen.py
@@ -3,10 +3,10 @@ from sys import exit
 
 
 class Screen():
-    def __init__(self, x, y, keys):
+    def __init__(self, width, height, magnitude, keys):
         pygame.init()
         self.clock = pygame.time.Clock()
-        self.screen = pygame.display.set_mode((x, y), 0, 32)
+        self.screen = pygame.display.set_mode((width * magnitude, height * magnitude), 0, 32)
         self.keys = keys
 
     def get_key(self):
@@ -17,6 +17,18 @@ class Screen():
                     for i, key in enumerate(self.keys):
                         if event.key == key:
                             return i
+
+    def key(self):
+        events = pygame.event.get()
+        for event in events:
+            if event.type == pygame.QUIT:
+                print('Quit request recieved! Exiting')
+                exit(0)
+            elif event.type == pygame.KEYDOWN:
+                for i, key in enumerate(self.keys):
+                    if event.key == key:
+                        return i
+        return None
 
     def keyevents(self):
         events = pygame.event.get()
@@ -35,7 +47,7 @@ if __name__ == '__main__':
             pygame.K_a, pygame.K_s, pygame.K_d, pygame.K_f,
             pygame.K_z, pygame.K_x, pygame.K_c, pygame.K_v]
 
-    screen = Screen(64, 32, keys)
+    screen = Screen(64, 32, 10, keys)
 
     while True:
         key = screen.get_key()
